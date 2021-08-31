edcdf7744b8506e5e36f4103f71972617d8f51ef
ilsu song <kdsww1@naver.com>
2021-08-31 13:52:50 +0900

    FBV로 블로그 포스트 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Post
+
+
+def index(request):
+    posts = Post.objects.all().order_by('-pk') # pk값을 역순으로 정렬
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts' : posts,
+        }
+    )
