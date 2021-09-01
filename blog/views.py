cd61aa8889d0853137a743c10d390cf1f7c06d74
ilsu song <kdsww1@naver.com>
2021-09-01 14:31:26 +0900

    CBC로 블로그 포스트 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+
 from .models import Post
 
+class PostList(ListView):
+    model = Post
+    ordering = '-pk' # pk값을 역순으로 정렬
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk') # pk값을 역순으로 정렬
+class PostDetail(DetailView):
+    model = Post
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts' : posts,
-        }
-    )
-
-def single_post_page(requst, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        requst,
-        'blog/single_post_page.html',
-        {
-            'post':post,
-        }
-    )
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk') # pk값을 역순으로 정렬
+#
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts' : posts,
+#         }
+#     )
+#
+# def single_post_page(requst, pk):
+#     post = Post.objects.get(pk=pk)
+#
+#     return render(
+#         requst,
+#         'blog/post_detail.html',
+#         {
+#             'post':post,
+#         }
+#     )
